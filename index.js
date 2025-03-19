b4d24304a5da4911ff5f67d1a0a9a170822a9236
Pedro Gonçalo <pedro.gonsalo939@gmail.com>
2025-03-18 21:43:55 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function toggleTheme() {
+    const body = document.body;
+    if (body.getAttribute('data-theme') === 'dark') {
+        body.removeAttribute('data-theme');
+    } else {
+        body.setAttribute('data-theme', 'dark');
+    }
+}
