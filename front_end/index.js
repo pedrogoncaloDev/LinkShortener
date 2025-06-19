0c54ab8726d7685660c0161955b6c5b8d83ba390
Pedro Gonçalo <pedro.gonsalo939@gmail.com>
2025-06-19 14:28:31 -0300

    feat: dark e light mode

--- front_end/index.js
+++ front_end/index.js
@@ -1,8 +1,8 @@
 function toggleTheme() {
     const body = document.body;
-    if (body.getAttribute('data-theme') === 'dark') {
+    if (body.getAttribute('data-theme') === 'light') {
         body.removeAttribute('data-theme');
     } else {
-        body.setAttribute('data-theme', 'dark');
+        body.setAttribute('data-theme', 'light');
     }
 }
